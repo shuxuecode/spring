63c21811e89f0cbdd5408b7995af9e0673a57eb5
zhaoshuxue <zhaoshuxue@163.com>
2021-02-03 20:03:41 +0800

    commitByZsx

--- scalaDemo/src/com/scala/ObjectDemo.scala
+++ scalaDemo/src/com/scala/ObjectDemo.scala
@@ -1,11 +1,33 @@
 package com.scala
 
+import java.util.ArrayList;
+
+
 object ObjectDemo {
 
   def main(args: Array[String]): Unit = {
-    println("Hello world!!!")
-  }
 
+    val str: String = "123";
+
+
+    println("Hello world!!!" + str)
+
+    var list: List[String] = List.empty
+
+    // ::在前面添加元素
+    list = list.::(str)
+
+    println(list.length)
+    println(list)
+
+    var arrayList: ArrayList[String] = new ArrayList();
+
+    arrayList.add(str)
+
+    println(arrayList)
+
+
+  }
 
 
 }
