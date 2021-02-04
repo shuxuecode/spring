ea969e2a9ce8998213da3a5584dc6ad5d40b555b
zhaoshuxue <zhaoshuxue@163.com>
2021-02-04 21:03:09 +0800

    commitByZsx

--- scalaDemoMaven/src/main/java/com/zsx/Test.scala
+++ scalaDemoMaven/src/main/java/com/zsx/Test.scala
@@ -1,5 +1,7 @@
 package com.zsx
 
+import org.apache.flink.api.java.ExecutionEnvironment
+
 /**
  * @Author zsx
  * @Date 2021/2/4
@@ -8,7 +10,15 @@ object Test {
 
 
   def main(args: Array[String]): Unit = {
-    println(1)
+
+
+    val env = ExecutionEnvironment.getExecutionEnvironment
+
+    val filePath = ""
+
+    val dataSet = env.readTextFile(filePath)
+
+
   }
 
 }
