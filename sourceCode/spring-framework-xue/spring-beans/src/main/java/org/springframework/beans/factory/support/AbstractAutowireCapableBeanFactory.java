721300b4cc27a7a9a98e8a218cdd68d9d59db6aa
zhaoshuxue <zhaoshuxue@163.com>
2021-03-16 18:38:25 +0800

    commitByZsx

--- sourceCode/spring-framework-xue/spring-beans/src/main/java/org/springframework/beans/factory/support/AbstractAutowireCapableBeanFactory.java
+++ sourceCode/spring-framework-xue/spring-beans/src/main/java/org/springframework/beans/factory/support/AbstractAutowireCapableBeanFactory.java
@@ -485,6 +485,7 @@ public abstract class AbstractAutowireCapableBeanFactory extends AbstractBeanFac
 		// Make sure bean class is actually resolved at this point, and
 		// clone the bean definition in case of a dynamically resolved Class
 		// which cannot be stored in the shared merged bean definition.
+		// todo mark
 		Class<?> resolvedClass = resolveBeanClass(mbd, beanName);
 		if (resolvedClass != null && !mbd.hasBeanClass() && mbd.getBeanClassName() != null) {
 			mbdToUse = new RootBeanDefinition(mbd);
--- sourceCode/spring-framework-xue/spring-beans/src/main/java/org/springframework/beans/factory/support/AbstractBeanFactory.java
+++ sourceCode/spring-framework-xue/spring-beans/src/main/java/org/springframework/beans/factory/support/AbstractBeanFactory.java
@@ -323,6 +323,7 @@ public abstract class AbstractBeanFactory extends FactoryBeanRegistrySupport imp
 				if (mbd.isSingleton()) {
 					sharedInstance = getSingleton(beanName, () -> {
 						try {
+							// todo mark
 							return createBean(beanName, mbd, args);
 						}
 						catch (BeansException ex) {
