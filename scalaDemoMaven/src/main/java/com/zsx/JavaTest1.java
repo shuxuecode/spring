92f1149a9574bd8399acb79ae30313531043d748
zhaoshuxue <zhaoshuxue@163.com>
2021-06-04 19:01:11 +0800

    commitByZsx

---
+++ scalaDemoMaven/src/main/java/com/zsx/JavaTest1.java
@@ -0,0 +1,106 @@
+package com.zsx;
+
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.java.DataSet;
+import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.api.java.utils.ParameterTool;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.util.Collector;
+
+/**
+ * flink 离线运行
+ *
+ * @author zsx
+ */
+public class JavaTest1 {
+    /*
+    public static void main(String[] args) throws Exception {
+        //构建环境
+        final ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+        //通过字符串构建数据集
+        DataSet<String> text = env.fromElements(
+                "Who's there?",
+                "I think I hear them. Stand, ho! Who's there?");
+        //分割字符串、按照key进行分组、统计相同的key个数
+        DataSet<Tuple2<String, Integer>> wordCounts = text
+                .flatMap(new LineSplitter())
+                .groupBy(0)
+                .sum(1);
+        //打印
+        wordCounts.print();
+    }
+
+    //分割字符串的方法
+    public static class LineSplitter implements FlatMapFunction<String, Tuple2<String, Integer>> {
+
+        public void flatMap(String line, Collector<Tuple2<String, Integer>> out) {
+            for (String word : line.split(" ")) {
+                out.collect(new Tuple2<String, Integer>(word, 1));
+            }
+        }
+    }
+    */
+
+
+    public static void main(String[] args) throws Exception {
+        //定义socket的端口号
+        int port;
+        try {
+            ParameterTool parameterTool = ParameterTool.fromArgs(args);
+            port = parameterTool.getInt("port");
+        } catch (Exception e) {
+            System.err.println("没有指定port参数，使用默认值9000");
+            port = 9000;
+        }
+        //获取运行环境
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        //连接socket获取输入的数据
+        DataStreamSource<String> text = env.socketTextStream("localhost", port, "\n");
+        //计算数据
+        DataStream<WordWithCount> windowCount = text.flatMap(new FlatMapFunction<String, WordWithCount>() {
+            public void flatMap(String value, Collector<WordWithCount> out) throws Exception {
+                String[] splits = value.split("\\s");
+                for (String word : splits) {
+                    out.collect(new WordWithCount(word, 1L));
+                }
+            }
+        })//打平操作，把每行的单词转为<word,count>类型的数据
+                //针对相同的word数据进行分组
+                .keyBy("word")
+                //指定计算数据的窗口大小和滑动窗口大小
+                .timeWindow(Time.seconds(2), Time.seconds(1))
+                .sum("count");
+        //把数据打印到控制台,使用一个并行度
+        windowCount.print().setParallelism(1);
+        //注意：因为flink是懒加载的，所以必须调用execute方法，上面的代码才会执行
+        env.execute("streaming word count");
+    }
+
+    /**
+     * 主要为了存储单词以及单词出现的次数
+     */
+    public static class WordWithCount {
+        public String word;
+        public long count;
+
+        public WordWithCount() {
+        }
+
+        public WordWithCount(String word, long count) {
+            this.word = word;
+            this.count = count;
+        }
+
+        @Override
+        public String toString() {
+            return "WordWithCount{" +
+                    "word='" + word + '\'' +
+                    ", count=" + count +
+                    '}';
+        }
+    }
+}
--- scalaDemoMaven/src/main/java/com/zsx/Test.scala
+++ scalaDemoMaven/src/main/java/com/zsx/Test.scala
@@ -1,6 +1,8 @@
 package com.zsx
 
+import org.apache.flink.api.common.functions.FlatMapFunction
 import org.apache.flink.api.java.ExecutionEnvironment
+import org.apache.flink.util.Collector
 
 /**
  * @Author zsx
@@ -8,15 +10,29 @@ import org.apache.flink.api.java.ExecutionEnvironment
  */
 object Test {
 
+  // TODO
 
   def main(args: Array[String]): Unit = {
 
 
+    // 初始化环境
     val env = ExecutionEnvironment.getExecutionEnvironment
 
-    val filePath = ""
+    val text = env.fromElements("abc", "bb", "ab")
 
-    val dataSet = env.readTextFile(filePath)
+//    val counts = text.flatMap {
+//      _.toLowerCase.split("\\W+") filter {
+//        _.nonEmpty
+//      }
+//    }
+//      .map {
+//        (_, 1)
+//      }
+//      .groupBy(0)
+//      .sum(1)
+//
+//    counts.print()
+
 
 
   }
